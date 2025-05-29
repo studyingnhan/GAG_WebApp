53f5d28b430d9db79126c408a971f6f756b107a1
Nhan Hoang <163379759+studyingnhan@users.noreply.github.com>
2025-05-29 17:02:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def index():
     return render_template("index.html", result=result, image_path=image_path)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
